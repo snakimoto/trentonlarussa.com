7d1e7a64cfcd55b8faa230a20852f2df92e60cb5
snakimoto <snakimoto@gmail.com>
2025-04-27 21:10:22 -0400

    Update roulette.js

--- roulette.js
+++ roulette.js
@@ -1,56 +1,68 @@
-// Setup
 let balance = 1000;
 let currentChip = 5;
-let bets = {}; // { number: amount }
+let bets = {};
 let spinning = false;
-let previousRolls = [];
+let sectors = [];
+const numbers = Array.from({length: 37}, (_, i) => i);
 
-// Wheel numbers (European style)
-const numbers = [
-  0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27,
-  13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1,
-  20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26
-];
+function generateWheel() {
+  const wheel = document.getElementById('wheel');
+  numbers.forEach((num, i) => {
+    const sector = document.createElement('div');
+    sector.className = 'wheel-sector';
+    sector.style.transform = `rotate(${i * (360/37)}deg) translate(120px) rotate(-${i * (360/37)}deg)`;
+    sector.innerText = num;
+    sector.id = `sector-${num}`;
 
-const colors = {
-  0: 'green', 32: 'red', 15: 'black', 19: 'red', 4: 'black', 21: 'red',
-  2: 'black', 25: 'red', 17: 'black', 34: 'red', 6: 'black', 27: 'red',
-  13: 'black', 36: 'red', 11: 'black', 30: 'red', 8: 'black', 23: 'red',
-  10: 'black', 5: 'red', 24: 'black', 16: 'red', 33: 'black', 1: 'red',
-  20: 'black', 14: 'red', 31: 'black', 9: 'red', 22: 'black', 18: 'red',
-  29: 'black', 7: 'red', 28: 'black', 12: 'red', 35: 'black', 3: 'red', 26: 'black'
-};
+    if (num === 0) {
+      sector.style.backgroundColor = 'green';
+    } else if ([1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36].includes(num)) {
+      sector.style.backgroundColor = 'red';
+    } else {
+      sector.style.backgroundColor = 'black';
+    }
 
-// Generate Betting Table
-const bettingTable = document.getElementById('betting-table');
+    wheel.appendChild(sector);
+    sectors.push(sector);
+  });
+}
 
-numbers.sort((a,b) => a-b).forEach(num => {
-  const div = document.createElement('div');
-  div.classList.add('bet-spot');
-  div.classList.add(colors[num]);
-  div.id = `spot-${num}`;
-  div.innerHTML = `<div>${num}</div><div class="bet-amount" id="amount-${num}"></div>`;
-  div.onclick = () => placeBet(num);
-  bettingTable.appendChild(div);
-});
+function generateBettingTable() {
+  const numberBets = document.getElementById('number-bets');
+  numbers.forEach(num => {
+    const div = document.createElement('div');
+    div.className = 'number-bet';
+    div.innerText = num;
+    div.onclick = () => placeBet(num);
+    numberBets.appendChild(div);
+  });
+}
 
 function selectChip(amount) {
   currentChip = amount;
 }
 
-function placeBet(num) {
+function placeBet(bet) {
   if (spinning) return;
-  if (balance >= currentChip) {
-    balance -= currentChip;
-    bets[num] = (bets[num] || 0) + currentChip;
-    updateBetDisplay(num);
-    updateBalance();
-  }
+  if (!bets[bet]) bets[bet] = 0;
+  bets[bet] += currentChip;
+  balance -= currentChip;
+  updateBalance();
+  updateLiveBets();
 }
 
-function updateBetDisplay(num) {
-  const amt = bets[num] || 0;
-  document.getElementById(`amount-${num}`).innerText = amt ? `$${amt}` : '';
+function placeSpecialBet(type) {
+  placeBet(type);
+}
+
+function updateLiveBets() {
+  const list = document.getElementById('live-bets-list');
+  list.innerHTML = '';
+  for (let bet in bets) {
+    const div = document.createElement('div');
+    div.innerText = `${bet}: $${bets[bet]}`;
+    list.appendChild(div);
+  }
 }
 
 function updateBalance() {
@@ -62,8 +74,7 @@ function reloadBalance() {
   balance = 1000;
   bets = {};
   updateBalance();
-  document.querySelectorAll('.bet-amount').forEach(e => e.innerText = '');
-  document.querySelectorAll('.bet-spot').forEach(e => e.classList.remove('orange'));
+  updateLiveBets();
 }
 
 function spinWheel() {
@@ -71,66 +82,75 @@ function spinWheel() {
   spinning = true;
   document.getElementById('result-box').innerText = 'Spinning...';
 
-  const ball = document.getElementById('ball');
-  let position = Math.floor(Math.random() * numbers.length);
-  let ticks = 0;
-  let totalTicks = 50 + Math.floor(Math.random() * 30); // 50-80 ticks
-  let speed = 50; // Initial speed
+  document.getElementById('spin-sound').play();
 
-  const moveBall = setInterval(() => {
-    position = (position + 1) % numbers.length;
-    const deg = (position / numbers.length) * 360;
-    ball.style.transform = `rotate(${deg}deg)`;
+  let position = 0;
+  let totalTicks = 60 + Math.floor(Math.random() * 20); // more ticks for smoother feel
+  let ticksLeft = totalTicks;
+  let speed = 30; // faster at first
 
-    ticks++;
+  const spin = setInterval(() => {
+    sectors.forEach(sec => sec.classList.remove('highlight-yellow', 'highlight-orange'));
+    sectors[position].classList.add('highlight-yellow');
 
-    if (ticks > totalTicks) {
-      clearInterval(moveBall);
-      landOn(numbers[position]);
+    position = (position + 1) % sectors.length;
+    ticksLeft--;
+
+    if (ticksLeft <= 0) {
+      clearInterval(spin);
+      const landedNumber = numbers[(position + sectors.length - 1) % sectors.length];
+      sectors[(position + sectors.length - 1) % sectors.length].classList.remove('highlight-yellow');
+      sectors[(position + sectors.length - 1) % sectors.length].classList.add('highlight-orange');
+
+      document.getElementById('ding-sound').play();
+
+      finishSpin(landedNumber);
     }
 
-    if (ticks > totalTicks * 0.7) speed += 10;
-
+    if (ticksLeft < totalTicks * 0.5) speed += 1; // slow down gradually
   }, speed);
 }
 
-function landOn(landedNum) {
-  const resultBox = document.getElementById('result-box');
-
-  document.querySelectorAll('.bet-spot').forEach(e => e.classList.remove('orange'));
-  document.getElementById(`spot-${landedNum}`).classList.add('orange');
-
-  previousRolls.unshift({num: landedNum, color: colors[landedNum]});
-  if (previousRolls.length > 10) previousRolls.pop();
-  updatePreviousRolls();
-
+function finishSpin(landedNumber) {
   let winAmount = 0;
-  if (bets[landedNum]) {
-    winAmount += bets[landedNum] * 35;
-  }
 
+  // Number win
+  if (bets[landedNumber]) winAmount += bets[landedNumber] * 35;
+
+  // Color win
+  const redNumbers = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36];
+  const blackNumbers = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35];
+  
+  if (bets['red'] && redNumbers.includes(landedNumber)) winAmount += bets['red'] * 2;
+  if (bets['black'] && blackNumbers.includes(landedNumber)) winAmount += bets['black'] * 2;
+  if (bets['green'] && landedNumber === 0) winAmount += bets['green'] * 36;
+
+  // Dozens win
+  if (bets['1st12'] && landedNumber >=1 && landedNumber <=12) winAmount += bets['1st12'] * 3;
+  if (bets['2nd12'] && landedNumber >=13 && landedNumber <=24) winAmount += bets['2nd12'] * 3;
+  if (bets['3rd12'] && landedNumber >=25 && landedNumber <=36) winAmount += bets['3rd12'] * 3;
+
+  // High/Low win
+  if (bets['low'] && landedNumber >=1 && landedNumber <=18) winAmount += bets['low'] * 2;
+  if (bets['high'] && landedNumber >=19 && landedNumber <=36) winAmount += bets['high'] * 2;
+
+  // Update balance and show result
   balance += winAmount;
   updateBalance();
 
   if (winAmount > 0) {
-    resultBox.innerText = `Ball landed on ${landedNum} (${colors[landedNum]}). You won $${winAmount}!`;
+    document.getElementById('result-box').innerText = `Ball landed on ${landedNumber}. You won $${winAmount}!`;
   } else {
-    resultBox.innerText = `Ball landed on ${landedNum} (${colors[landedNum]}). You lost.`;
+    document.getElementById('result-box').innerText = `Ball landed on ${landedNumber}. No win this time.`;
   }
 
+  // Reset bets
   bets = {};
-  document.querySelectorAll('.bet-amount').forEach(e => e.innerText = '');
+  updateLiveBets();
   spinning = false;
 }
 
-function updatePreviousRolls() {
-  const container = document.getElementById('previous-rolls-list');
-  container.innerHTML = '';
-  previousRolls.forEach(roll => {
-    const div = document.createElement('div');
-    div.classList.add('roll-circle');
-    div.style.backgroundColor = roll.color;
-    div.innerText = roll.num;
-    container.appendChild(div);
-  });
-}
+window.onload = function() {
+  generateWheel();
+  generateBettingTable();
+};
